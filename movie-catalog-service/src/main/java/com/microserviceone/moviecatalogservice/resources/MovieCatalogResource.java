9f60780376d4dd895cbff809b5c01cd8609465a7
ketansahu07 <ketankumar120@gmail.com>
2022-01-16 18:47:07 +0530

    catalog calling movies api

--- movie-catalog-service/src/main/java/com/microserviceone/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/microserviceone/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,12 +1,15 @@
 package com.microserviceone.moviecatalogservice.resources;
 
 import com.microserviceone.moviecatalogservice.models.CatalogItem;
+import com.microserviceone.moviecatalogservice.models.Movie;
+import com.microserviceone.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,8 +17,16 @@ public class MovieCatalogResource {
 
   @RequestMapping("/{userId}")
   public List<CatalogItem> getcatalog(@PathVariable("userId") String userId) {
-    return Collections.singletonList(
-          new CatalogItem("The Fantastic Beast", "A memorable movie for me", 5)
+
+    RestTemplate restTemplate = new RestTemplate();
+    List<Rating> ratings = List.of(
+          new Rating("1234", 4),
+          new Rating("5678", 5)
     );
+
+    return ratings.stream().map(rating -> {
+      Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+      return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+    }).collect(Collectors.toList());
   }
 }
